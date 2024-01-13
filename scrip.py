e6a36c9f5e987f18ed3a5ed023b73e8fedfcf95b
mindmaster17 <127671399+mindmaster17@users.noreply.github.com>
2024-01-13 22:12:30 +0530

    Update scrip.py
    Editted the file by adding more comments

--- scrip.py
+++ scrip.py
@@ -3,7 +3,6 @@ import time
 
 banner_1 = """
                       __●__ ●
-                      _ █___█
                       __ █__ █_
                       __ █__ █
                       __ ███____________█████ 　　　
@@ -14,12 +13,11 @@ banner_1 = """
                       ____█▒░██ ██▓▒░░ ████▓█
                       ___█▒░██__██▓▓▒▒░░░██ 　 ★★
                       ____█▒░██___████████████
-                      _____█▒░█▒▒▒▒▒▒▒▒▒▒▒▒█
                       ______██████████████████.•°*”˜҈.•°*”˜҈.                                  
                                           
 
 """
-
+# Displays an Ascii Text Banner
 print(banner_1)
 path_1 = Path.cwd()
 print("Running File: ", path_1)
@@ -27,10 +25,12 @@ seconds = time.time()
 local_time = time.ctime(seconds)
 print("Local Time:", local_time)
 
+# Input fron the user
 string_1 = input("String: ")
 text_1 = input("Text: ")
 file_1 = open("/Users/gthea/OneDrive/Documents/file_1.txt", "w")
 
+# Generating different links
 for i in range(10):
     text_2 = text_1 + str(i)
     position_1 = string_1.find(text_1)
