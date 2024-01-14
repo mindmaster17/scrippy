b649152b29570f7c55e2218b9ad4a2da63051f90
mindmaster17 <127671399+mindmaster17@users.noreply.github.com>
2024-01-14 20:17:42 +0530

    Create scrip version_0.1.py
    Added image download Script

---
+++ scrip version_0.1.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import time
+import urllib.request 
+from PIL import Image 
+
+banner_1 = """
+                      __●__ ●
+                      _ █___█
+                      __ █__ █
+                      __ ███____________█████ 　　　
+                      _█▒░░█_________██▓▒▒▓██ ☆
+                      █▒░●░░█___ ██▓▒██▓▒▒▓█　　 ★
+                      █░█▒░░██_ ██▓▒██▓▒░▒▓█
+                      _██▒░░██ ██▓▒░██▓▒░▒▓█ 　　　★
+                      ____█▒░██ ██▓▒░░ ████▓█
+                      ___█▒░██__██▓▓▒▒░░░██ 　 ★★
+                      ____█▒░██___████████████
+                      ______██████████████████.•°*”˜҈.•°*”˜҈.                                  
+                                          
+
+"""
+
+print(banner_1)
+path_1 = Path.cwd()
+print("Running File: ", path_1)
+seconds = time.time()
+local_time = time.ctime(seconds)
+print("Local Time:", local_time)
+
+file_name = "img_"
+string_1 = input("String: ")
+text_1 = input("Text: ")
+pages = int(input("No. Pages: "))
+
+file_1 = open("/Users/gthea/OneDrive/Documents/file_1.txt", "w")
+
+for i in range(10):
+    file_name_1 = file_name + str(i) + ".png"
+    text_2 = text_1 + str(i)
+    position_1 = string_1.find(text_1)
+    replaced_1 = string_1.replace(text_1,text_2) 
+    print(replaced_1)
+    file_1.write(str(replaced_1 + "\n"))
+    urllib.request.urlretrieve(replaced_1, file_name_1 ) 
+
+img = Image.open('img_1.jpg') 
+img.show()
+    
+file_1.close()
